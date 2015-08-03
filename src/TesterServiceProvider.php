c60e9cd503c3d63f1999da2c5aba1e58573bb937
Rafel Sansó Monserrat <rafel.sanso@gmail.com>
2015-08-03 16:07:34 +0200

    - Testing Travis

--- src/TesterServiceProvider.php
+++ src/TesterServiceProvider.php
@@ -22,18 +22,11 @@ class TesterServiceProvider extends ServiceProvider {
     public function boot()
     {
         // Fix for PSR-4
-        //$this->package('jenssegers/ab', 'ab', realpath(__DIR__));
         $this->loadViewsFrom(realpath(__DIR__), 'ab');
         
         $this->publishes([
             realpath(__DIR__).'/config/config.php' => config_path('ab.php'),
         ]);
-
-        // Start the A/B tracking when routing starts.
-        /*$this->app->before(function($request)
-        {
-            $this->app['ab']->track($request);
-        });*/
     }
     
     /**
@@ -44,7 +37,6 @@ class TesterServiceProvider extends ServiceProvider {
     public static function setTrack($request) {
         $service = new TesterServiceProvider;
         $service->app['ab']->track($request);
-        //return true;
     }
 
     /**
