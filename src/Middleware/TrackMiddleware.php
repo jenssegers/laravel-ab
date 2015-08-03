bd1807a3580648304ae9557fe65fa4616826357e
Gary <gary@ahead4.com>
2015-08-03 17:09:20 +0100

    Space indents

--- src/Middleware/TrackMiddleware.php
+++ src/Middleware/TrackMiddleware.php
@@ -5,34 +5,34 @@ use Jenssegers\AB\Tester;
 
 class TrackMiddleware {
 
-	/**
-	 * AB Tester.
-	 *
-	 * @var \Jenssegers\AB\Tester
-	 */
-	protected $ab;
+    /**
+     * AB Tester.
+     *
+     * @var \Jenssegers\AB\Tester
+     */
+    protected $ab;
 
-	/**
-	 * Start A/B tracking middleware.
-	 *
-	 */
-	public function __construct()
-	{
-		$this->ab = app('ab');
-	}
+    /**
+     * Start A/B tracking middleware.
+     *
+     */
+    public function __construct()
+    {
+        $this->ab = app('ab');
+    }
 
-	/**
-	 * Handle request.
-	 *
-	 * @param  \Illuminate\Http\Request $request
-	 * @param  Closure $next
-	 * @return mixed
-	 */
-	public function handle($request, Closure $next)
-	{
-		$this->ab->track($request);
+    /**
+     * Handle request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $this->ab->track($request);
 
-		return $next($request);
-	}
+        return $next($request);
+    }
 
 }
